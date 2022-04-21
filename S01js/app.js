06cdc29d65e51f7c6bf03dabd274e575ad197c79
erhan <erhantezer68@gmail.com>
2022-04-21 23:03:28 +0300

    second commit

--- S01js/app.js
+++ S01js/app.js
@@ -1,6 +1,10 @@
-console.log("hello erhan");
+// console.log("hello erhan");
 
-/* let ad = prompt("adınızı girin");
-console.log(ad);
-console.log("adım:", ad)
-console.log("adım = " + ad) */
+//  let ad = prompt("adınızı girin");
+// console.log(ad);
+// console.log("adım:", ad)
+// console.log("adım = " + ad) */
+// let sonuc = confirm("eminmisiniz");
+// console.log(sonuc);
+// document.write("erhan bugün bize geldi");
+// console.log(`adınız:${ad} dır.`)
