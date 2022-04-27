206123c80757708b15848116a0e8f9086d92adf7
erhan <erhantezer68@gmail.com>
2022-04-28 02:49:30 +0300

    added

--- S02js/app.js
+++ S02js/app.js
@@ -76,14 +76,40 @@
 // console.log(program);
 
 
+//    ARTIK YIL ÇÖZÜMÜ 1   
 
-const year = 2152;
-const month = 0;
-let dayCount;
- switch()
+// const year = new Date().getFullYear();
+// const month = 2;
+// let dayCount;
+//  switch(month){
 
+// case 1:
+// case 3:
+// case 5:
+// case 7:
+// case 8:
+// case 10:
+// case 12:
+// dayCount = 31;
+// break
 
-console.log(dayCount);
+// case 4:
+// case 6:
+// case 9:
+// case 11:
+//     dayCount = 30;
+//     break
+// case 2:
+//     if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+//     dayCount = 29;
+//     else dayCount = 28;
+//     break;
+// default:
+//     dayCount = -1;
+
+//  }
+
+// console.log(dayCount);
 
 
 
