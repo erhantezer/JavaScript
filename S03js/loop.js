af5f12deb618b44b8d8fafc2a23beef5733526ba
Erhan <erhantezer68@gmail.com>
2022-05-10 21:33:13 +0300

    added

---
+++ S03js/loop.js
@@ -0,0 +1,25 @@
+console.log("*****Loops*****");
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log("Merhaba", i);
+// }
+// for (let i = 1; i <= 10; i++) {
+//     const rdmSayi = Math.floor(Math.random() * 100 + 1);
+//     console.log(rdmSayi);
+// }
+
+
+// const sayi = prompt("sayı giriniz : ");
+// let asal = 0;
+// for (let i = 2; i < sayi;) {
+//     if (sayi % i == 0) {
+//         asal++;
+//     }
+//     i++;
+// }
+// if (asal == 0) {
+//     console.log("asal ");
+// } else {
+//     console.log("asal değil");
+// }
