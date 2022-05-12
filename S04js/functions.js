92ad4d60c475fc1e2330f8722d44a0a28d90e507
Erhan <erhantezer68@gmail.com>
2022-05-12 21:39:36 +0300

    added

---
+++ S04js/functions.js
@@ -0,0 +1,29 @@
+
+// function yazdır(){
+//     console.log("merhaba");
+// }
+// yazdır();
+// console.log(typeof yazdır);
+
+
+
+// function selamla(name, lastname=''){
+//     console.log(`Merhaba ${name} ${lastname}`);
+// }
+// selamla('Erhan', 'TEZER');
+// selamla('Erol');
+// selamla('Jason');
+
+
+// function yasHesapla(year, name){
+//     const mesaj=`${name}'in yaşı ${new Date().getFullYear()-year} dir` ;
+//     return mesaj
+// }
+//  const mesaj1 = yasHesapla(2001, "Elif");
+//  console.log(mesaj1);
+
+
+// function tekCift(number){
+//    return number % 2 === 0 ? "çift" : "tek"
+// }
+// console.log(tekCift(5));
