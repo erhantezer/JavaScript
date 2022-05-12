060e9fcbd2d9445313c4025ebd706468f12598ba
Erhan <erhantezer68@gmail.com>
2022-05-12 23:19:41 +0300

    added

--- S04js/functions.js
+++ S04js/functions.js
@@ -27,3 +27,36 @@
 //    return number % 2 === 0 ? "çift" : "tek"
 // }
 // console.log(tekCift(5));
+
+
+
+
+
+//  const tekCift=function (number){
+//    return number % 2 === 0 ? "çift" : "tek"
+// }
+// console.log(tekCift(5));
+
+
+// const bul = function (x, y, z){
+//     let enbüyük;
+//     if(x>=y && x>=z){
+//         enbüyük= x;
+//     }
+//     else if(y>=x && y>=z){
+//         enbüyük= y;
+//     }
+//     else if(z>=y && z>=x){
+//         enbüyük= z;
+//     }else{
+//         enbüyük= x;
+//     }
+// return enbüyük;
+// }
+// console.log('En büyük sayı:', bul(1,3,2));
+
+
+
+
+const topla = (x, y) => x + y;
+console.log(topla(3, 4))
